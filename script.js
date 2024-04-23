9c2b1e2cb6428d6708d945bf5e5cbcd0c8ddfea4
Gian Carlo Fandialan <gianchifandialan03@gmail.com>
2024-04-23 19:36:57 +0200

    COMMENTATO

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const ciao = document.getElementById("ciao");
 
 buttonSearch.addEventListener("click", function(search) {
     search = searchId.value;
+
     fetch(`https://api.pexels.com/v1/search?query=${search}`, 
         {headers: {
             authorization: "ZyQrutNF9uvRtaS7930OvYBowhf5Wuzmy8JPIVOddA3WYhOAmTKORqHs"
@@ -17,10 +18,16 @@ buttonSearch.addEventListener("click", function(search) {
         .then(response => {return response.json()})
 
         .then(data => {
+            //ESTRAGGO L'ARRAY
             array = data.photos;
-            console.log(array);
+
+            //CREO LA VARIABILE PER ROW
             const row = document.querySelector(".row");
+
+            //CREO UN NUOVO ARRAY VUOTO IN CUI ANDRO AD INSERIRE GLI ELEMENTI FILTRATI
             let filtered = [];
+
+            //FILTRO IN BASE A QUALE RADIO è STATA CLICCATA
             if (btnradio1.checked || btnradio2.checked) {
                 if (btnradio1.checked) {
                     filtered = array.filter(element => element.height < 3000)
@@ -31,17 +38,28 @@ buttonSearch.addEventListener("click", function(search) {
                 filtered = array;
             }
 
+            //CREO UN NUOVO SINISTO CON MAP SOLO DELL'ALTEZZA DELLE IMMAGINI
             let map = filtered.map(elemento => elemento.height);
+
+            //USO IL REDUCE PER FARE LA SOMMA DELLE ALTEZZE MAPPATE
             let somma = map.reduce((curr,num) => curr + num, 0);
+
+            //INSERISCO IL VALORE DI SOMMA NELL'HTML
             ciao.innerText = `Total height: ${somma}`;
 
+            //CREO LA CARD PER OGNI ELEMENTO FILTRATO
             filtered.forEach(element => {
+
+                //CREO LA CARD
                 let card = document.createElement("div");
+
+                //AGGIUNGO LE CLASSI PER STILIZZARE LA CARD
                 card.classList.add("card")
                 card.classList.add("col")
                 card.classList.add("p-0")
                 card.style.width = "18rem";
 
+                //INSERISCO IL CONTENUTO DELLA CARD ESTRAENDOLO DA OGNI ELEMENTO DELL'ARRAY FILTRATO CHE è UN OGGETTO
                 card.innerHTML =
                 `
                 <img src="${element.src.original}" class="card-img-top" alt="${element.alt} style="height: 5rem;">
@@ -55,6 +73,8 @@ buttonSearch.addEventListener("click", function(search) {
                     <li class="list-group-item"><small class="fs-6">id: ${element.id}</small></li>
                 </ul>
                 `
+
+                //INSERISCO LA CARD ALLA ROW
                 row.appendChild(card);
                 });
 
